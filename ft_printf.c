0b67a6c28a39b6fa17807bb48979d64a0e0f6d08
Paola Danielle Ferrete <pferrete@c2r8s3.42lisboa.com>
2023-05-04 19:22:23 +0100

    add ftprintf

--- ft_printf.c
+++ ft_printf.c
@@ -6,72 +6,53 @@
 /*   By: pferrete <pferrete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 12:00:14 by pferrete          #+#    #+#             */
-/*   Updated: 2023/05/03 19:54:18 by pferrete         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:12:21 by pferrete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_checktype(int c, const char *s)
-{
-	if (c == 's')
-		ft_putstr_fd(c, 1);
-	else if (c == 'd')
-		ft_putnbr_fd(c, 1);
-	else if (c == 'c')
-		ft_putchar_fd(c, 1);
-	else if (c == 'p')
-	else if (c == 'd')
-	else if (c == 'i')
-	else if (c == 'u')
-	else if (c == 'x')
-	else if (c == 'X')
-	else
-		'%'
-}
+#include <stdarg.h>
 
-void  ft_putchar_fd(int s)
+void ft_putchar_fd(int s)
 {
     write(1, &s, 1);
 }
 
+void ft_checktype(const char type, va_list args)
+{
+    int   c;
 
-void  ft_checktype(const char *format, va_list args)
-{3
+    if (type == 'c')
+      ft_putchar_fd(args);
+    else
+        printf("%s", "nada");
+    return;
+}
+
+int ft_printf(const char *format, ...)
+{
     int i;
 
     i = 0;
+    va_list args;
+    va_start(args, format);
     while (format[i])
     {
         if (format[i] == '%')
+        {
+            ft_checktype(format[++i], va_arg(args, int));
             i++;
-        else if (format[i] == 'c')
-        {
-            char ch = va_arg(args, int);
-            ft_putchar_fd(ch);
-        }
-        else if (format[i] == 's')
-        {
-            const char *s = va_arg(args, const char *)
         }
         else
-            printf("%s", "nada");
-        i++;
+            ft_putchar_fd(format[i]);
     }
-    return ;
-}
 
-void    ft_printf(const char *format, ...)
-{
-
-    va_list args;
-    va_start (args, format);
-    ft_checktype(*format, args);
     va_end(args);
+    return (0);
 }
 
-int    main(void)
+int main(void)
 {
-    char    *farray = "o";
+    char *farray = "o";
 
     ft_printf("%c", farray);
 }
-
